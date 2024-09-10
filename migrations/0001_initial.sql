a5d04ab8201eaa4a433418037f324679e6a9ef32
Mads Mogensen <mail@madsmogensen.dk>
2024-09-10 17:54:47 +0200

    Feature: Add Initial DB Schema and test user endpoints
    Call localhost:8080/api/user/create
    It should display "Ok"
    
    Then call localhost:8080/api/user/all
    
    It should display a user as html in an ol element.

---
+++ migrations/0001_initial.sql
@@ -0,0 +1,58 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(128) NOT NULL,
+  email VARCHAR(128) NOT NULL,
+  notes VARCHAR NOT NULL,
+  join_timestamp TIMESTAMPTZ NOT NULL DEFAULT now(),
+
+  UNIQUE(username),
+  UNIQUE(email)
+);
+
+CREATE TABLE products (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(128) NOT NULL,
+  price BIGINT NOT NULL CONSTRAINT nonnegative_price CHECK(price >= 0),
+  active BOOLEAN NOT NULL,
+  deactivate_after_timestamp TIMESTAMPTZ DEFAULT NULL
+);
+
+CREATE TABLE product_aliases (
+  product_id SERIAL NOT NULL,
+  alias_name VARCHAR(128) NOT NULL,
+
+  UNIQUE(product_id, alias_name),
+
+  CONSTRAINT fk_product 
+    FOREIGN KEY(product_id)
+      REFERENCES products(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE sales (
+  id SERIAL PRIMARY KEY NOT NULL,
+  price BIGINT NOT NULL CONSTRAINT nonnegative_price CHECK(price >= 0),
+  timestamp TIMESTAMPTZ NOT NULL DEFAULT now(),
+  product_id SERIAL NOT NULL,
+  user_id SERIAL NOT NULL,
+  
+  CONSTRAINT fk_product
+    FOREIGN KEY(product_id)
+      REFERENCES products(id),
+
+  CONSTRAINT fk_user
+    FOREIGN KEY(user_id)
+      REFERENCES users(id)
+);
+
+CREATE TABLE deposits (
+  id SERIAL PRIMARY KEY NOT NULL,
+  amount BIGINT NOT NULL CONSTRAINT nonnegative_amount CHECK(amount > 0),
+  timestamp TIMESTAMPTZ NOT NULL,
+  note VARCHAR NOT NULL,
+  user_id SERIAL NOT NULL,
+
+  CONSTRAINT fk_user
+    FOREIGN KEY(user_id)
+      REFERENCES users(id)
+);
