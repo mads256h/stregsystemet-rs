b73ae3b2c53a3eb052a889603d125621652879b7
Mads Mogensen <mail@madsmogensen.dk>
2024-09-20 12:54:44 +0200

    Quick-buy executor: Check product inactivity and add tests
    Add fixtures with sample data to added in the database when used in tests.
    Check that product is active.
    Add tests.

---
+++ fixtures/product_aliases.sql
@@ -0,0 +1,8 @@
+INSERT INTO product_aliases(alias_name, product_id)
+VALUES
+  ('enabled',  1),
+  ('active',   1),
+  ('inactive', 3),
+  ('inactive_timestamp', 4),
+  ('expensive', 5),
+  ('overflow', 6);
---
+++ fixtures/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(id, username, email, notes)
+VALUES
+  (1, 'test_user', 'test@email.com', 'test user');
