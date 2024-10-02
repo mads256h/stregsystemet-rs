c8cb59b985b5f750462b4d7c69c7143dd7627c3d
Mads Mogensen <mail@madsmogensen.dk>
2024-10-02 20:35:36 +0200

    Make quickbuy a form
    This gives us submit query when pressing enter for free.
    
    Format main.js

--- static/main.js
+++ static/main.js
@@ -1,31 +1,36 @@
+"use strict";
+
 document.addEventListener("DOMContentLoaded", initializePage);
 
 async function initializePage() {
-    addQuickBuyHandler();
+  addQuickBuyHandler();
 
-    try {
-        const activeProducts = await getActiveProducts();
-        populateTable(activeProducts);
-    }
-    catch (error) {
-        console.error(error.message);
-    }
+  try {
+    const activeProducts = await getActiveProducts();
+    populateTable(activeProducts);
+  }
+  catch (error) {
+    console.error(error.message);
+  }
 }
 
 function addQuickBuyHandler() {
-  const quickBuyButton = document.getElementById("quickbuy-submit-button");
-  console.assert(quickBuyButton);
+  const quickBuyForm = document.getElementById("quickbuy");
+  console.assert(quickBuyForm);
 
-  quickBuyButton.addEventListener("click", performQuickBuy);
+  quickBuyForm.addEventListener("submit", performQuickBuy);
 }
 
-async function performQuickBuy() {
+async function performQuickBuy(e) {
+  // Stop the default form submission logic
+  e.preventDefault();
+
   const quickBuyInput = document.getElementById("quickbuy-field");
   console.assert(quickBuyInput);
 
 
   const url = "/api/purchase/quickbuy";
-  const response = await fetch(url, 
+  const response = await fetch(url,
     {
       method: "POST",
       headers: {
@@ -39,44 +44,44 @@ async function performQuickBuy() {
 }
 
 async function getActiveProducts() {
-    const url = "/api/products/active";
-    const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-    }
+  const url = "/api/products/active";
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
 
-    const json = await response.json();
-    console.log(json);
-    return json.content.products;
+  const json = await response.json();
+  console.log(json);
+  return json.content.products;
 }
 
 function populateTable(products) {
-    const table1 = document.getElementById("products1").getElementsByTagName("tbody")[0];
-    const table2 = document.getElementById("products2").getElementsByTagName("tbody")[0];
-    for (const i in products) {
-        const row = createRow(products[i]);
-        if (i % 2 == 0) {
-            table1.appendChild(row);
-        } else {
-            table2.appendChild(row);
-        }
-
+  const table1 = document.getElementById("products1").getElementsByTagName("tbody")[0];
+  const table2 = document.getElementById("products2").getElementsByTagName("tbody")[0];
+  for (const i in products) {
+    const row = createRow(products[i]);
+    if (i % 2 == 0) {
+      table1.appendChild(row);
+    } else {
+      table2.appendChild(row);
     }
+
+  }
 }
 
 function createRow(product) {
-    const row = document.createElement("tr")
-    const id = createTableCell(product.id);
-    const name = createTableCell(product.name);
-    const price = createTableCell(product.price);
-    row.appendChild(id);
-    row.appendChild(name);
-    row.appendChild(price);
-    return row;
+  const row = document.createElement("tr")
+  const id = createTableCell(product.id);
+  const name = createTableCell(product.name);
+  const price = createTableCell(product.price);
+  row.appendChild(id);
+  row.appendChild(name);
+  row.appendChild(price);
+  return row;
 }
 
 function createTableCell(text) {
-    const cell = document.createElement("td")
-    cell.innerText = text;
-    return cell;
+  const cell = document.createElement("td")
+  cell.innerText = text;
+  return cell;
 }
