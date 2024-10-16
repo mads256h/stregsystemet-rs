3b39d27869fd0fa7cc0dee1f8852ccf1a49f479e
henneboy <henrik_peet@hotmail.com>
2024-10-16 20:14:02 +0200

    Add windows scripts

---
+++ scripts/init_db.bat
@@ -0,0 +1,2 @@
+cargo sqlx db create
+cargo sqlx migrate run
---
+++ scripts/start_postgres.bat
@@ -0,0 +1,1 @@
+docker run -d --name "postgres-stregsystemet" -l "postgres-stregsystemet" -e POSTGRES_USER="stregsystemet" -e POSTGRES_PASSWORD="password" -e POSTGRES_DB="stregsystemet" -p 5432:5432 docker.io/postgres:16
